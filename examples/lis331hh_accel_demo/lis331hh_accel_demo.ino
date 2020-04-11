9dd5e295ffde13b4dd0b456dd63478f908c17572
siddacious <nospam187+github@gmail.com>
2020-04-10 18:49:51 -0700

    fixing example

--- examples/lis331hh_accel_demo/lis331hh_accel_demo.ino
+++ examples/lis331hh_accel_demo/lis331hh_accel_demo.ino
@@ -26,7 +26,7 @@ void setup(void) {
 
   Serial.println("LIS331HH test!");
 
-  if (! lis.begin(0x18)) {   // change this to 0x19 for alternative i2c address
+  if (! lis.begin_I2C()) {   // change this to 0x19 for alternative i2c address
     Serial.println("Couldnt start");
     while (1) yield();
   }
