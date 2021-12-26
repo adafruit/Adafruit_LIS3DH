43320dfa37ceeb5a9ab98bb9d861bd77ef5061da
Giulio Berti <giukio@gmail.com>
2021-12-26 15:52:59 +0100

    Added example for temperature readings

---
+++ examples/tempdemo/tempdemo.ino
@@ -0,0 +1,52 @@
+// Basic demo for temperature readings from Adafruit LIS3DH
+
+#include <Wire.h>
+#include <SPI.h>
+#include <Adafruit_LIS3DH.h>
+#include <Adafruit_Sensor.h>
+
+// Used for software SPI
+#define LIS3DH_CLK 13
+#define LIS3DH_MISO 12
+#define LIS3DH_MOSI 11
+// Used for hardware & software SPI
+#define LIS3DH_CS 10
+
+// software SPI
+//Adafruit_LIS3DH lis = Adafruit_LIS3DH(LIS3DH_CS, LIS3DH_MOSI, LIS3DH_MISO, LIS3DH_CLK);
+// hardware SPI
+//Adafruit_LIS3DH lis = Adafruit_LIS3DH(LIS3DH_CS);
+// I2C
+Adafruit_LIS3DH lis = Adafruit_LIS3DH();
+
+void setup(void) {
+#ifndef ESP8266
+  while (!Serial) yield();     // will pause Zero, Leonardo, etc until serial console opens
+#endif
+
+  Serial.begin(9600);
+    Serial.println("Adafruit LIS3DH ADC Test!");
+  
+  if (! lis.begin(0x18)) {   // change this to 0x19 for alternative i2c address
+    Serial.println("Couldnt start");
+    while (1) yield();
+  }
+  Serial.println("LIS3DH found!");
+  
+  lis.setRange(LIS3DH_RANGE_2_G);   // 2, 4, 8 or 16 G!
+  
+  Serial.print("Range = "); Serial.print(2 << lis.getRange());  
+  Serial.println("G");
+}
+
+
+void loop() {
+
+  // LIS3DH measures a Delta T with respect to an unspecified temperature
+  // Pass your best guess of the reference temperature as argument.
+  int8_t temp = lis.readTemperature(13);
+  Serial.print("Temp: "); Serial.print(temp);  Serial.print(" °C");
+
+  Serial.println();
+  delay(200);
+}
