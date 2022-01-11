38a4f17e9b477de4741ffa6a0425c346c28e08e8
Doxygen CI <ci-arduino@invalid>
2022-01-11 18:06:33 +0000

    Deploy docs to GitHub Pages from commit fc5f9d2f85
    Commit: fc5f9d2f859e7b9f7fbad05c12995add79695207
    GitHub Actions run: 1683896022

--- html/search/functions_5.js
+++ html/search/functions_5.js
@@ -1,5 +1,6 @@
 var searchData=
 [
   ['read',['read',['../class_adafruit___l_i_s3_d_h.html#a7f3bb9fd26fef61971b01d961f7d923c',1,'Adafruit_LIS3DH']]],
-  ['readadc',['readADC',['../class_adafruit___l_i_s3_d_h.html#aa326ca8d436ebe2d87501ae4c985fb6d',1,'Adafruit_LIS3DH']]]
+  ['readadc',['readADC',['../class_adafruit___l_i_s3_d_h.html#aa326ca8d436ebe2d87501ae4c985fb6d',1,'Adafruit_LIS3DH']]],
+  ['readandclearinterrupt',['readAndClearInterrupt',['../class_adafruit___l_i_s3_d_h.html#a9745ed39d5e87d665c133693646e6574',1,'Adafruit_LIS3DH']]]
 ];
